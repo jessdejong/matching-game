f9389dcc2c54ee43e1145064da3d2e8659b055a2
Jess DeJong <jess@Jesss-MacBook-Pro.local>
2023-01-06 19:07:19 -0600

    firebase config update

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { addDoc, collection, getDocs, getFirestore, orderBy, query } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,18 +7,18 @@ import { addDoc, collection, getDocs, getFirestore, orderBy, query } from "fireb
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB8nCp0LfEopF4oROM8FK5jaPyf8x0z2zY",
-  authDomain: "matching-game-with-firebase.firebaseapp.com",
-  projectId: "matching-game-with-firebase",
-  storageBucket: "matching-game-with-firebase.appspot.com",
-  messagingSenderId: "820320672499",
-  appId: "1:820320672499:web:8d206dc2e443a1f5f2b586",
-  measurementId: "G-ZGDR68MBDS",
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID ,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 async function getLeaderboard(db) {
@@ -30,18 +29,11 @@ async function getLeaderboard(db) {
   let result = [];
   querySnapshot.forEach((doc) => {
     result.push(doc.data());
-    // console.log(doc.id, " => ", doc.data());
   });
 
   console.log(result);
   
   return result;
-
-  // console.log("here");
-  // const gameRuns = collection(db, "gameRuns");
-  // const gameRunSnapshot = await getDocs(gameRuns);
-  // const gameRunsList = gameRunSnapshot.docs.map((doc) => doc.data());
-  // return gameRunsList;
 }
 
 async function addGameRun(db, gameRunObj) {
